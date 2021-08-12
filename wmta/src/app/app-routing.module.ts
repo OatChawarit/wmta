121b99b42e930d57bd5fec13bdcd8b16583991f0
(100231) ชวลิต พรหมลัทธิ์ <100231@mnl.local>
2021-08-12 20:03:23 +0700

    oat

--- wmta/src/app/app-routing.module.ts
+++ wmta/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './home/login/login.component';
 
 const routes: Routes = [
+  { path: '',component: LoginComponent},
   { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'login',component: LoginComponent},
-  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)}
+  { path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
+  { path: 'doctor', loadChildren: () => import('./doctors/doctors.module').then(m => m.DoctorsModule)},
 ];
 
 @NgModule({
--- wmta/src/app/pages/pages.module.ts
+++ wmta/src/app/pages/pages.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PagesRoutingModule } from './pages-routing.module';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
+import { MediaCsrComponent } from './media-csr/media-csr.component';
 
 
 @NgModule({
   declarations: [
     AboutUsComponent,
-    ContactUsComponent
+    ContactUsComponent,
+    MediaCsrComponent
   ],
   imports: [
     CommonModule,
